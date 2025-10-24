319a9d2fde82961f11981f6791a9a7b9c6e2b244
DiegoRamos1012 <diegorsantos09@outlook.com>
2025-10-24 08:57:33 -0300

    Configurando roteamento no totem-admin

--- totem-admin/src/App.tsx
+++ totem-admin/src/App.tsx
@@ -1,56 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// src/App.tsx
+import React, { Suspense } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <div className="max-w-7xl mx-auto p-8 text-center">
-        <div className="flex justify-center gap-4">
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img
-              src={viteLogo}
-              className="h-[6em] p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
-              style={{ willChange: "filter" }}
-              alt="Vite logo"
-            />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img
-              src={reactLogo}
-              className="h-[6em] p-6 transition duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] motion-safe:animate-[spin_20s_linear_infinite]"
-              style={{ willChange: "filter" }}
-              alt="React logo"
-            />
-          </a>
-        </div>
-
-        <h1 className="mt-6 text-2xl font-semibold">Vite + React</h1>
-
-        <div className="card p-8 mt-6 bg-gray-800 rounded-lg shadow-sm">
-          <button
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-            onClick={() => setCount((c) => c + 1)}
-          >
-            count is {count}
-          </button>
-          <p className="mt-4">
-            Edit{" "}
-            <code className="bg-gray-700 text-white px-1 rounded">
-              src/App.tsx
-            </code>{" "}
-            and save to test HMR
-          </p>
-        </div>
-
-        <p className="read-the-docs mt-6 text-gray-300">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Login />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ totem-admin/src/routes/routes.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import { BrowserRouter } from "react-router";
+import RoutesApp from "./routes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+  );
+}
+
+export default App;
